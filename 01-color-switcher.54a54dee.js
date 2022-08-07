a5a868025649cd8d2ebcf39280d6d889045871ae
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2022-08-07 12:06:30 +0000

    Deploying to gh-pages from @ AlesyaMazurenko/goit-js-hw-09@e2970f4b7f27b64f092601f82dc98ba61cd58dc0 🚀

--- 01-color-switcher.54a54dee.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0,t.addEventListener("click",(function(){e.removeAttribute("disabled"),t.disabled=!0;setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){t.removeAttribute("disabled"),e.disabled=!0,clearInterval(timerId)}))}();
-//# sourceMappingURL=01-color-switcher.54a54dee.js.map
